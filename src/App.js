74622a03b2c95a800a539ca1f82269b1233345d4
goorm <goorm@goormui-MacBookPro-5.local>
2025-01-02 00:05:14 +0900

    feature: posts페이지 UI구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login.js";
 import Regist from "./pages/auth/Regist.js";
+import Posts from "./pages/Posts.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/regist" element={<Regist />} />
+          <Route path="/posts" element={<Posts />} />
         </Routes>
     </div>
     </BrowserRouter>
---
+++ src/pages/Posts.js
@@ -0,0 +1,66 @@
+import BackButton from "../components/BackButton";
+import TitleHeader from "../components/TItleHeader";
+import Post from "../components/Post";
+import "../styles/Posts.css"
+import Lottie from "react-lottie";
+import animationData from "../assets/headerBackgroundAnim.json"; // JSON 파일 경로
+import ToggleProfile from "../components/ToggleProfile";
+
+const Posts = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+
+    return(
+        <div className="postsBox">
+                <Lottie
+                    options={defaultOptions}
+                    width={700}
+                    height={104} // postsHeaders 높이와 맞춤
+                    style={{
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        zIndex: -1,
+                    }} // 애니메이션 배경 설정
+                />
+            <div className="postsHeaders">
+                <div className="postsHeader">
+                    <BackButton/>
+                    <TitleHeader/>
+                    <ToggleProfile/>
+                </div>
+            </div>
+            <main>
+                <div class="mainTxt">
+                    <p id="mainTxt">안녕하세요,</p>
+                    <p id="mainTxt">아무 말 대잔치 <strong>게시판</strong> 입니다.</p>
+                </div>
+                <div class="bnt">
+                    <div class="postBtn" id="postBtn">
+                        <p><strong>게시물 작성</strong></p>
+                    </div>
+                </div>
+                <div class="postList">
+                    {/* <!--  게시물 리스트 ------> */}
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
+
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Posts;
